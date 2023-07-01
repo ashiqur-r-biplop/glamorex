245d6e7b84f76433e2d1745d8299410c1ca40c6e
Abtahi Hasan <101439174+AbtahiHasan@users.noreply.github.com>
2023-06-30 22:41:58 -0700

    basic setup done

---
+++ src/provider/ReduxProvider.js
@@ -0,0 +1,11 @@
+import store from "@/redux-toolkit/store";
+import { Provider } from "react-redux";
+
+
+const ReduxProvider = ({children}) => {
+    return (
+        <Provider store={store}>{children}</Provider>
+    )
+};
+
+export default ReduxProvider;
---
+++ src/redux-toolkit/slice/featureProduct/productSlice.js
@@ -0,0 +1,9 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const productSlice = createSlice({
+    name: 'featureProducts',
+    initialState: [],
+
+})
+
+export default productSlice
