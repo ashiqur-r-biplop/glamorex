5e85f28845ec541bf9e880942fd350429e63c767
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-07-03 14:16:59 +0600

    component updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: {
     remotePatterns: [
       {
@@ -10,17 +9,5 @@ const nextConfig = {
     ],
   },
 };
-=======
-    images: {      
-            remotePatterns: [
-              {
-                protocol: 'https',
-                hostname: 'images.pexels.com',
-              },
-            ],
-
-    }
-}
->>>>>>> a1fe5f54a91808182534de62577e41bcf6013eb7
 
 module.exports = nextConfig;
