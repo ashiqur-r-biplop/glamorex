dc6f617bc51b7a56e60f24a61de84a9491ad0d3b
aothymoon59 <mmoon8745@gmail.com>
2023-07-05 16:46:42 +0600

    profile page done

---
+++ src/app/(customers)/account/[id]/page.jsx
@@ -0,0 +1,147 @@
+"use client";
+import UpdateButton from "../../components/account/UpdateButton";
+import { useForm } from "react-hook-form";
+
+const User = {
+  id: "123",
+  displayName: "Agun",
+  email: "test@gmail.com",
+  photoURL: "https://i.ibb.co/37cdWvc/images.jpg",
+  mobile: "+8801700000000",
+  birthday: "2002-01-15",
+  gender: "male",
+};
+
+const ProfileEdit = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      gender: User?.gender,
+    },
+  });
+
+  const onSubmit = (data) => {
+    const { name, email, photo, mobile, birthday, gender } = data;
+    const updatedProfile = {
+      name,
+      email,
+      photo,
+      mobile,
+      birthday,
+      gender,
+    };
+    console.log(updatedProfile);
+  };
+
+  return (
+    <div className="container mx-auto px-5 py-[100px] min-h-[70vh]">
+      <div className="section-title mb-8">
+        <h2 className="text-2xl font-semibold text-center">Update Profile</h2>
+      </div>
+      <div className="border-2 border-gray-100 px-5 py-10 flex flex-col xl:flex-row gap-12 xl:gap-20 md:w-fit mx-auto">
+        <div>
+          <img
+            className="w-[200px] h-[200px] rounded-full mx-auto border"
+            src={User?.photoURL}
+            alt={User?.displayName}
+          />
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10">
+            <div className=" space-y-1">
+              <label>
+                <span className="font-semibold text-lg">Name</span>
+              </label>
+              <input
+                type="text"
+                {...register("name", { required: true })}
+                defaultValue={User?.displayName}
+                className="border p-2 rounded-sm w-full"
+              />
+            </div>
+            <div className=" space-y-1">
+              <label>
+                <span className="font-semibold text-lg">Email</span>
+              </label>
+              <input
+                type="text"
+                {...register("email", { required: true })}
+                defaultValue={User?.email}
+                readOnly
+                className="border p-2 rounded-sm w-full"
+              />
+            </div>
+            <div className=" space-y-1">
+              <label>
+                <span className="font-semibold text-lg">Photo URL</span>
+              </label>
+              <input
+                type="text"
+                {...register("photo", { required: true })}
+                defaultValue={User?.photoURL}
+                className="border p-2 rounded-sm w-full"
+              />
+            </div>
+            <div className=" space-y-1">
+              <label>
+                <span className="font-semibold text-lg">Mobile</span>
+              </label>
+              <input
+                type="text"
+                {...register("mobile", { required: true })}
+                defaultValue={User?.mobile}
+                className="border p-2 rounded-sm w-full"
+              />
+            </div>
+            <div className=" space-y-1">
+              <label>
+                <span className="font-semibold text-lg">Birthday</span>
+              </label>
+              <input
+                type="date"
+                {...register("birthday", { required: true })}
+                defaultValue={User?.birthday}
+                className="border p-2 rounded-sm w-full"
+              />
+            </div>
+            <div className=" space-y-1">
+              <label>
+                <span className="font-semibold text-lg">Gender</span>
+              </label>
+              <div className="flex gap-2">
+                <label>
+                  <input
+                    type="radio"
+                    value="male"
+                    {...register("gender", { required: true })}
+                  />
+                  Male
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="female"
+                    {...register("gender", { required: true })}
+                  />
+                  Female
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="other"
+                    {...register("gender", { required: true })}
+                  />
+                  Other
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="mt-5 flex gap-3">
+            <UpdateButton type="submit">Save Changes</UpdateButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileEdit;
--- src/app/(customers)/account/page.jsx
+++ src/app/(customers)/account/page.jsx
@@ -1,7 +1,59 @@
+import Link from "next/link";
+import EditButton from "../components/account/EditButton";
+
+const User = {
+  id: "123",
+  displayName: "Agun",
+  email: "test@gmail.com",
+  photoURL: "https://i.ibb.co/37cdWvc/images.jpg",
+};
+
 const AccountPage = () => {
   return (
-    <div>
-      <h2> AccountPage Aothy mahamud Moon</h2>
+    <div className="container mx-auto px-5 py-[100px] min-h-[70vh]">
+      <div className="section-title mb-8">
+        <h2 className="text-2xl font-semibold text-center">My Profile</h2>
+      </div>
+      <div className="border-2 border-gray-100 px-5 py-10 flex flex-col xl:flex-row gap-12 xl:gap-20 md:w-fit mx-auto">
+        <div>
+          <img
+            className="w-[200px] h-[200px] rounded-full mx-auto"
+            src={User?.photoURL}
+            alt={User?.displayName}
+          />
+        </div>
+        <div className="my-profile">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10">
+            <div>
+              <p className="font-semibold text-lg mb-1">Name</p>
+              <h4>{User?.displayName}</h4>
+            </div>
+            <div>
+              <p className="font-semibold text-lg mb-1">Email</p>
+              <h4>{User?.email}</h4>
+            </div>
+            <div>
+              <p className="font-semibold text-lg mb-1">Mobile</p>
+              <h4>
+                {User?.birthday ? User?.birthday : "mobile not available"}
+              </h4>
+            </div>
+            <div>
+              <p className="font-semibold text-lg mb-1">Birthday</p>
+              <h4>{User?.mobile ? User?.mobile : "birthday not available"}</h4>
+            </div>
+            <div>
+              <p className="font-semibold text-lg mb-1">Gender</p>
+              <h4>{User?.gender ? User?.gender : "gender not available"}</h4>
+            </div>
+          </div>
+          <div className="mt-5">
+            <Link href={`/account/${User?.id}`}>
+              <EditButton>Edit Profile</EditButton>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
