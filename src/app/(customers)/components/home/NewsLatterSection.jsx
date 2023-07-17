ac19a4d1ff3d2daecbe001d3a93d1de429b26167
aothymoon59 <mmoon8745@gmail.com>
2023-07-17 12:25:13 +0600

    about us card,dveloper card and favorite add

--- src/app/(customers)/components/home/NewsLatterSection.jsx
+++ src/app/(customers)/components/home/NewsLatterSection.jsx
@@ -1,9 +1,13 @@
+import CmnSectionTitle from "../HelpingCompo/CmnSectionTitle";
 import NewsletterCard from "./cards/NewsletterCard";
 
 const NewsLatterSection = () => {
   return (
     <div className="max-w-screen-2xl mx-auto sm:px-6 md:px-8 px-4 my-10">
-      <p className="text-center text-4xl font-semibold mb-5">NewsLetter!</p>
+      <CmnSectionTitle
+        title={"Newsletter"}
+        subtitle={"Explore our Unique Selection and Exclusive Offers"}
+      ></CmnSectionTitle>
       <NewsletterCard />
     </div>
   );
