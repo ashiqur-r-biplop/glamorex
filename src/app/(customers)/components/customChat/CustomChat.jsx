f339cf43859a4659e86fe9a0a42b4d4da50e00a7
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-07-21 23:37:20 +0600

    added massageing feature

---
+++ src/app/(customers)/components/customChat/CustomChat.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from "react";
+import { FacebookProvider, CustomChat } from 'react-facebook';
+
+const FbMassage = () => {
+  return (
+    <>
+      <FacebookProvider appId="807819170938646" chatSupport>
+        <CustomChat pageId="109149734066082" minimized={true} />
+      </FacebookProvider>
+    </>
+  );
+};
+
+export default FbMassage;
