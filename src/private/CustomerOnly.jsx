3581fd8b035aebb53ccdb9d063d50b372967221a
aothymoon59 <mmoon8745@gmail.com>
2023-07-22 15:13:11 +0600

    navbar wishlist added

--- src/private/CustomerOnly.jsx
+++ src/private/CustomerOnly.jsx
@@ -3,24 +3,14 @@ import useAuth from '@/hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import Loading from './Loading';
 import useUserRole from '@/hooks/useUserRole';
-<<<<<<< HEAD
-=======
 import LoadingSpinner from '@/app/(customers)/components/HelpingCompo/LoadingSpinner';
->>>>>>> 36734b78bd4000ad5b7d095eadf239ad9d680eb7
 
 const CustomerOnly = ({ children }) => {
     const router = useRouter()
-<<<<<<< HEAD
-    const { role, loading: userRoleLoading } = useUserRole();
-    const {loading, user} = useAuth()
-    if(loading) {
-        return <Loading/>
-=======
     const { loading, user } = useAuth()
     const { role, loading: roleLoading } = useUserRole()
     if (loading || roleLoading) {
         return <LoadingSpinner></LoadingSpinner>
->>>>>>> 36734b78bd4000ad5b7d095eadf239ad9d680eb7
     }
     if (user) {
         if (role === "customer") {
