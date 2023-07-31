e948c1b4b5a970aafc0410aecc5bc4b83561d125
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-07-31 23:38:29 +0600

    change some

--- src/app/(customers)/page.jsx
+++ src/app/(customers)/page.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
       <TrustedSection />
       <Testimonials />
       <NewsLatterSection />
-      <GiftWheel />
+      {/* <GiftWheel /> */}
       <FbMassage />
     </>
   );
